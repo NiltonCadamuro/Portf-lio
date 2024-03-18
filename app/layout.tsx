d2133311f3216d18b52fd16e923e8022ece3e486
NiltonCadamuro <nilton.jrcadamuro@hotmail.com>
2024-03-18 17:20:08 -0300

    feat: google tag manager implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,9 +28,25 @@ export default function RootLayout({
           gtag('config', 'G-YE8YRDQ4XE');
         `}
       </Script>
+      <Script id="google-tag-manager" strategy="lazyOnload">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MKSH4Q8Z');
+          `}
+      </Script>
 
       <ModeProvider>
-        <body className={`${heebo.className}`}>{children}</body>
+        <body className={`${heebo.className}`}>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKSH4Q8Z" height="0" width="0" style="display:none visibility:hidden"><iframe>`,
+            }}
+          />
+          {children}
+        </body>
       </ModeProvider>
     </html>
   );
